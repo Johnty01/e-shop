91441dfb012cd0d266c041b7a242ab9f5cf927fe
Johnty01 <vibhusdf123@gmail.com>
2023-11-09 00:01:03 +0530

    product APIs and routes added

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,8 +1,12 @@
 const express = require("express")
-const { getAllProducts } = require("../controllers/productController")
+const { getAllProducts,createProduct,updateProduct, deleteProduct, getProductDetails } = require("../controllers/productController")
 
 const router = express.Router()
 
 router.route("/products").get(getAllProducts)
+router.route("/product/new").post(createProduct)
+router.route("/product/:id").put(updateProduct).delete(deleteProduct).get(getProductDetails)//cannot keep products/:id because it will understand both route same
+//routers clash in node with same string before /id
+//so if first route has /product then it can clash with third route
 
 module.exports = router
