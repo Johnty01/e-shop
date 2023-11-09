6da4a24c546f5576711bd0dd3d67b4f285553e68
Johnty01 <vibhusdf123@gmail.com>
2023-11-09 23:23:36 +0530

    uncaught error handling

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,14 @@ const app = require("./app")
 const dotenv = require("dotenv")
 
 const connectDatabase = require("./config/database")
+
+//Handling Uncaught Exception
+process.on("uncaughtException",(err)=>{
+    console.log(`Error: ${err.message}`)
+    console.log(`shutting down the server due to uncaught exception`)
+    process.exit(1)
+})
+
 //config
 
 dotenv.config({path: "backend/config/config.env"});
@@ -15,7 +23,7 @@ const server = app.listen(process.env.PORT,
     ()=>{
         console.log(`Server is starting on http://localhost:${process.env.PORT}`)
     })
-
+//console.log(randomVar) // uncaught exception example
 //Unhandled Promise Rejection
 process.on("unhandledRejection",err=>{
     console.log(`Error: ${err.message}`)
